7ac68292e238f3e548c39719b0d46d1a3244c624
Allxy <allxy01@gmail.com>
2022-11-19 00:47:55 +0300

    feat: form submit

--- src/components/FoodAdder.js
+++ src/components/FoodAdder.js
@@ -1,22 +1,51 @@
 import React from "react";
 import "./FoodAdder.css";
 
-class FoodAdder extends React.Component {
+export default class FoodAdder extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {FoodList : []};
+    super(props);
+    this.state = {
+      food: "",
+      calories: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit() {}
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleSubmit(this.state.food, this.state.calories);
+    this.setState({
+      food: "",
+      calories: "",
+    });
+  };
 
   render() {
     return (
-      <form className="food-adder">
-        <input type="text" placeholder="Добавьте продукты" />
+      <form className="food-adder" onSubmit={this.handleSubmit}>
+        <input
+          name="food"
+          onChange={this.handleChange}
+          value={this.state.food}
+          type="text"
+          required
+          placeholder="Добавьте продукты"
+        />
+        <input
+          name="calories"
+          onChange={this.handleChange}
+          value={this.state.calories}
+          type="number"
+          required
+          placeholder="Калории"
+        />
         <button className="food-adder__submit"></button>
       </form>
     );
   }
 }
-
-export default FoodAdder;
