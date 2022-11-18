50e0a5025f5645f207c62669a4aaa97750f94ef0
Allxy <allxy01@gmail.com>
2022-11-18 18:16:08 +0300

    feat: routing and tips page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Diary from './Diary';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Diary from "./Diary";
+import Tips from "./Tips";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Diary />
-    </>
+      <Routes>
+        <Route path="/" element={<Diary />} />
+        <Route path="/tips" element={<Tips />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
